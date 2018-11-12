9d9408194d4c19ae58c8f6afafa6605483f86794
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 19:10:00 -0600

    update editorconfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,34 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    exec: {
-      test: 'npm test',
-      cucumber: 'bundle exec cucumber',
+    grunt.initConfig({
+        exec: {
+            test: 'npm test',
+            cucumber: 'bundle exec cucumber',
+            jshint: 'node node_modules/jshint/bin/jshint .',
+            jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;',
+            eslint: 'node node_modules/eslint/bin/eslint .',
+            aspelllint: 'bundle exec aspelllint',
+            editorconfig: 'git ls-files -z | grep -av patch | xargs -0 -r -n 100 eclint check'
+        }
+    });
 
-      jshint: 'node node_modules/jshint/bin/jshint .',
-      jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;',
-      eslint: 'node node_modules/eslint/bin/eslint .',
-      aspelllint: 'bundle exec aspelllint',
-      lili: 'bundle exec lili .',
-      editorconfig: 'flcl . | xargs -n 100 editorconfig-cli check'
-    }
-  });
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('default', ['exec:test']);
+    grunt.registerTask('test', ['exec:test']);
+    grunt.registerTask('cucumber', ['exec:cucumber']);
 
-  grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('lint', [
+        'exec:jshint',
+        // Go to bed, Crockford
+        // 'exec:jslint',
+        'exec:eslint',
+        'exec:editorconfig'
+    ]);
 
-  grunt.registerTask('default', ['exec:test']);
-  grunt.registerTask('test', ['exec:test']);
-  grunt.registerTask('cucumber', ['exec:cucumber']);
-
-  grunt.registerTask('lint', [
-    'exec:jshint',
-    // Go to bed, Crockford
-    // 'exec:jslint',
-    'exec:eslint',
-    'exec:lili',
-    'exec:editorconfig'
-  ]);
-
-  grunt.registerTask('jshint', ['exec:jshint']);
-  grunt.registerTask('jslint', ['exec:jslint']);
-  grunt.registerTask('eslint', ['exec:eslint']);
-  grunt.registerTask('aspelllint', ['exec:aspelllint']);
-  grunt.registerTask('lili', ['exec:lili']);
-  grunt.registerTask('editorconfig', ['exec:editorconfig']);
+    grunt.registerTask('jshint', ['exec:jshint']);
+    grunt.registerTask('jslint', ['exec:jslint']);
+    grunt.registerTask('eslint', ['exec:eslint']);
+    grunt.registerTask('aspelllint', ['exec:aspelllint']);
+    grunt.registerTask('editorconfig', ['exec:editorconfig']);
 };
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,11 +1,11 @@
 Given(/^the program has finished$/) do
-  @cucumber = `npm test`
+    @cucumber = `npm test`
 end
 
 Then(/^the output is correct for each test$/) do
-  lines = @cucumber.split("\n")
+    lines = @cucumber.split("\n")
 
-  expect(lines.length).to eq(5)
+    expect(lines.length).to eq(5)
 
-  expect(lines[-1]).to match(%r(^@[0-9]{3}\.[0-9]{2}$))
+    expect(lines[-1]).to match(%r(^@[0-9]{3}\.[0-9]{2}$))
 end
